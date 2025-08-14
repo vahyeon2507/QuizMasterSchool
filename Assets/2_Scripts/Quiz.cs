72b804c04aa30b551cf24e6184371b72b65e792e
gkasuals <vahyeon2511@sonline20.sen.go.kr>
2025-08-14 12:58:06 +0900

    text

--- Assets/2_Scripts/Quiz.cs
+++ Assets/2_Scripts/Quiz.cs
@@ -5,9 +5,15 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField]  TextMeshProUGUI questionText;
     [SerializeField]  QuestionSO questions;
+    [SerializeField]  TextMeshProUGUI[] answersText;   
     void Start()
     {
-        questionsText[0].text = questions.GetAnswer(0);
+        questionText.text = questions.GetAnswer(0);
+
+        for (int i = 0; i < answersText.Length; i++)
+        {
+            answersText[i].text = questions.GetAnswer(i);
+        }
     }
 
     void Update()
